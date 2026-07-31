5d89846089ac476d0e1bf469910789e7a37b143b
znebcrumbs <150631723+znebcrumbs@users.noreply.github.com>
2026-07-31 12:58:17 +0800

    deployment bug

--- api/index.py
+++ api/index.py
@@ -1,3 +1,10 @@
+from pathlib import Path
+import sys
+
+ROOT_DIR = Path(__file__).resolve().parents[1]
+if str(ROOT_DIR) not in sys.path:
+    sys.path.insert(0, str(ROOT_DIR))
+
 from config.wsgi import application
 
 app = application
